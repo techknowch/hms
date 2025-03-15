6571739a73525345db91cbb5feb499e287b7dc0c
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-15 17:35:59 +0500

    Endpoint to create patient

--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Put,
+    Delete,
+    NotFoundException
+} from '@nestjs/common';
+import { PatientService } from './patient.service';
+import { Patient } from 'src/entities/patient.entity';
 
 @Controller('patient')
-export class PatientController {}
+export class PatientController {
+    constructor(
+        private readonly patientService: PatientService
+    ) { }
+
+    @Post()
+    async create(@Body() patientData: Partial<Patient>): Promise<Patient> {
+        return this.patientService.create(patientData);
+    }
+
+}
