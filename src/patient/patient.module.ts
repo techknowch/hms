0ac372cdcc882718068cc96b3e160651fc24191d
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-15 17:39:27 +0500

    Import Patient entity and TypeOrmModule in Patient module

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PatientController } from './patient.controller';
 import { PatientService } from './patient.service';
+import { Patient } from 'src/entities/patient.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Patient])],
   controllers: [PatientController],
   providers: [PatientService]
 })
-export class PatientModule {}
+export class PatientModule { }
