0bb071444992c864fac3513aa4886b32532f85fe
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-15 17:21:31 +0500

    Endpoint to create patient

--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Patient } from 'src/entities/patient.entity';
 
 @Injectable()
-export class PatientService {}
+export class PatientService {
+    constructor(
+        @InjectRepository(Patient)
+        private readonly patientRepository: Repository<Patient>,
+    ) { }
+
+    async create(patientData: Partial<Patient>): Promise<Patient> {
+        const patient = this.patientRepository.create(patientData);
+        return this.patientRepository.save(patient);
+    }
+}
