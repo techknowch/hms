52eefc7fb58ec9a9641b53dffc7ab0a4e962ff01
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-15 17:15:26 +0500

    Setup DB connection and import patient module and entity in main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
+import { Patient } from './entities/patient.entity';
 
 @Module({
-  imports: [PatientModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'alinaqvi',
+      password: 'dev',
+      database: 'healthcare_db',
+      entities: [Patient],
+      synchronize: true
+    }),
+    PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
