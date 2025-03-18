0b0f203897e3803e1d479225288e566cc1a2191b
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-18 22:45:34 +0500

    Add logger to log invalid api key validation

--- src/common/middleware/api-key/api-key.middleware.ts
+++ src/common/middleware/api-key/api-key.middleware.ts
@@ -1,18 +1,22 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+// src/common/middleware/api-key.middleware.ts
+import { Injectable, NestMiddleware, UnauthorizedException, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(ApiKeyMiddleware.name);
+
   constructor(private readonly configService: ConfigService) { }
 
   use(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.headers['x-api-key'];
-    const validApiKey = this.configService.get<string>('API_KEY'); // Get API key from environment
+    const validApiKey = this.configService.get<string>('API_KEY');
 
     if (apiKey === validApiKey) {
       next();
     } else {
+      this.logger.warn(`Invalid API key attempt from IP: ${req.ip}`); // Log the invalid attempt
       throw new UnauthorizedException('Invalid API key');
     }
   }
