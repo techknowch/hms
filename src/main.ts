1a63925c7cd28df02be24951d520affd88e07a66
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-08 01:21:52 +0500

    Install nest swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Health Management System API')
+    .setDescription('API documentation for Health Management System')
+    .setVersion('1.0')
+    .addTag('Health Management System')
+    .addBearerAuth()  // Enable JWT support (if using authentication)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
